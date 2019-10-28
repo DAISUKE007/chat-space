d9737c0b6c91aa274296a9ff69f00bd381e634c1
DAISUKE007 <da1suke.haseee@gmail.com>
2019-10-28 21:36:09 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -22,29 +22,31 @@ $(function(){
     return html;
 
   }
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    // var url = window.location.href + '';
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
+  $(document).on('turbolinks:load', function() {
+    $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action')
+      // var url = window.location.href + '';
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html)
+        $('#new_message')[0].reset();
+        $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight});
+        $('.form__submit').removeAttr('disabled');
+      })
+      .fail(function(){
+        alert('エラー');
+        $('.form__submit').removeAttr('disabled');
+      })
     })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html)
-      $('#new_message')[0].reset();
-      $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight});
-      $('.form__submit').removeAttr('disabled');
-    })
-    .fail(function(){
-      alert('エラー');
-      $('.form__submit').removeAttr('disabled');
-    })
-  })
+  });
 });
