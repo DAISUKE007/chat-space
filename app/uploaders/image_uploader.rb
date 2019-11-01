37e86b776156caada335ba9488be209e337f8983
DAISUKE007 <da1suke.haseee@gmail.com>
2019-11-01 16:12:35 +0900

    自動デプロイ実装作業２

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,10 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_fit: [800, 800]
+
+  storage :fog
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 end
